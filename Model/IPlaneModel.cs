f74a3630463c879a5c3d9453b495903de0fe0cf2
yehonatansofri <58885453+yehonatansofri@users.noreply.github.com>
2020-03-24 18:44:50 +0200

    added comments

--- Model/IPlaneModel.cs
+++ Model/IPlaneModel.cs
@@ -1,22 +1,31 @@
-﻿using System.ComponentModel;
+using System.ComponentModel;
 
 namespace PlaneController.Model
-{
-    //add documentation and explnation about methods (e.g. left + right -)
+{ 
+    /*
+     * Interface for a plane that serves as a model in MVVM architecture.
+     * In order if implementors to work correctly,
+     * Plane must be connected to a socket.
+     */
     interface IPlaneModel : INotifyPropertyChanged
     {
-        
-        //for plane server
-        void Connect(string ip, int port);
+
+        // Connect to plane in the given ip and port.
+        void Connect(string ip, string port);
+
+        // Disconnect from plane.
         void Disconnect();
 
-        //mitzeret
+        // Valid values are [0, 1].
         void SetThrottle(double value);
-        //meaznot
+        
+        // Valid values are [-1, 1].
         void SetAileron(double value);
-        //hege gova
+        
+        // Valid values are [-1, 1].
         void SetElevator(double value);
-        //hege kivun
+
+        // Valid values are [-1, 1].
         void SetRudder(double value);
     }
 }
