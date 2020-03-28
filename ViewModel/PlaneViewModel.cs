e6e518a2935d7dbb012e07bc6b6677f91589ac60
yehonatansofri <58885453+yehonatansofri@users.noreply.github.com>
2020-03-28 19:10:09 +0300

    added viewmodel

---
+++ ViewModel/PlaneViewModel.cs
@@ -0,0 +1,50 @@
+﻿using PlaneController.Model;
+using System.ComponentModel;
+
+namespace PlaneController.ViewModel
+{
+    class PlaneViewModel : INotifyPropertyChanged
+    {
+        private IPlaneModel _model;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public double VM_HeadingDeg { get { return _model.HeadingDeg; } }
+        public double VM_GPSVerticalSpeed { get { return _model.GPSVerticalSpeed; } }
+        public double VM_GPSGroundSpeed { get { return _model.GPSGroundSpeed; } }
+        public double VM_GPSAltitude { get { return _model.GPSAltitude; } }
+        public double VM_PitoSpeed { get { return _model.PitoSpeed; } }
+        public double VM_PitoAltitude { get { return _model.PitoAltitude; } }
+        public double VM_Roll { get { return _model.Roll; } }
+        public double VM_Pitch { get { return _model.Pitch; } }
+        public double VM_Latitude { get { return _model.Latitude; } }
+        public double VM_Longitude { get { return _model.Longitude; } }
+
+        public PlaneViewModel(IPlaneModel model)
+        {
+            _model = model;
+            _model.PropertyChanged +=
+                delegate (object sender, PropertyChangedEventArgs e)
+            {
+                // Add "VM_" only to properties, not errors.
+                foreach (var propInfo in this.GetType().GetProperties())
+                {
+                    if (propInfo.Name == e.PropertyName)
+                    {
+                        NotifyPropertyChanged("VM_" + e.PropertyName);
+                        return;
+                    }
+                }
+
+                NotifyPropertyChanged(e.PropertyName);
+            };
+        }
+
+        public void NotifyPropertyChanged(string message)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(message));
+            }
+        }
+    }
+}
