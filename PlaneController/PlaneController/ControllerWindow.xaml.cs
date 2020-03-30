ccf232d8293ce3e54b84d00296dbaf6f0cae882f
ronyut <ronyut@gmail.com>
2020-03-30 09:40:01 +0300

    Controller window + map is done and bound + joystick only gui (no animation)

--- PlaneController/PlaneController/ControllerWindow.xaml.cs
+++ PlaneController/PlaneController/ControllerWindow.xaml.cs
@@ -1,28 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using PlaneController.ViewModel;
+using PlaneController.Model;
 
 namespace PlaneController
 {
-    /// <summary>
-    /// Interaction logic for ControllerWindow.xaml
-    /// </summary>
-    public partial class ControllerWindow : Window
-    {
-        public ControllerWindow(string ip, string port)
-        {
-            InitializeComponent();
+   public partial class ControllerWindow : Window
+   {
+      public ControllerWindow(string ip, string port)
+      {
+         InitializeComponent();
+         PlaneViewModel vm = new PlaneViewModel(new PlaneModel());
+         DataContext = vm;
+         BottomSlider.Value = 0.5;
+      }
 
-        }
-    }
+      private void BottomSlider_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+      {
+         BottomSlider.Value = 0;
+      }
+
+      private void LeftSlider_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+      {
+         LeftSlider.Value = 0;
+      }
+
+      private void Joystick_Loaded(object sender, RoutedEventArgs e)
+      {
+
+      }
+   }
 }
