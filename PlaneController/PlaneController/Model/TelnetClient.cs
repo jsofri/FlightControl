82c503a730e008f4d170472a6d086fc370bc4885
yehonatansofri <58885453+yehonatansofri@users.noreply.github.com>
2020-03-30 15:01:48 +0300

    paste from my project

--- PlaneController/PlaneController/Model/TelnetClient.cs
+++ PlaneController/PlaneController/Model/TelnetClient.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -6,6 +6,16 @@ using System.Threading;
 
 namespace PlaneController.Model
 {
+
+    /*
+     * Send and recieve messages on a telnet protocol to a specific end point.
+     * Use a known set of messages and execute lambda after each message.
+     * Use a MessageQueue object to send "set" messages to end point.
+     * Make sure a send won't take more than 10 seconds to execute.
+     * 
+     * author: Jhonny.
+     * data: 3.28.20
+     */
     class TelnetClient
     {
 
@@ -129,10 +139,6 @@ namespace PlaneController.Model
                 // To empty buffer.
                 int bytesRec = _sender.Receive(bytes);
             }
-            catch (ObjectDisposedException)
-            {
-                NotifyErrorHappened("Socket closed");
-            }
             catch (Exception)
             {
                 NotifyErrorHappened("ERR");
@@ -164,10 +170,6 @@ namespace PlaneController.Model
             catch (FormatException)
             {
                 NotifyErrorHappened("NaN");
-            }
-            catch (ObjectDisposedException)
-            {
-                NotifyErrorHappened("Socket closed");
             }
             catch (Exception)
             {
