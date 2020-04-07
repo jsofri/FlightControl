46e681ef7af032c295064a975e632efa572db6b5
yehonatansofri <ysofri@gmail.com>
2020-04-07 21:44:21 +0300

    first attempt

---
+++ PlaneController/ControllerWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.Maps.MapControl.WPF;
+using PlaneController.ViewModel;
+using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Media;
+
+namespace PlaneController
+{
+
+    /*
+     * Class is a control window in a plane simulator.
+     * Function as a View class in MVVM architecture.
+     * 
+     * author: Jhonny.
+     * date: 4.1.20
+     */
+    public partial class ControllerWindow : Window
+    {
+        private volatile PlaneViewModel _vm;
+
+        private double _throttle;
+
+        private double _aileron;
+
+        Pushpin AirplanePin = new Pushpin();
+
+
+        // Ctor - gets a ViewModel object as a parameter.
+        public ControllerWindow(PlaneViewModel vm)
+        {
+            InitializeComponent();
+            _vm = vm;
+            this.DataContext = this._vm;
+            _throttle = ThrottleSlider.Value = 0.5;
+            _aileron = AileronSlider.Value = 0;
+
+            // subscribe 
+            _vm.PropertyChanged += UpdateCoordsPin;
+            this.SetErrorEvent();
+
+            // pushpin
+            AirplanePin.Location = new Location(vm.VM_Latitude, vm.VM_Longitude);
+            AirplanePin.ToolTip = "Airplane";
+            AirplanePin.Margin = new Thickness(0, 0, 0, 37);
+            this.myMap.Children.Add(AirplanePin);
+        }
+
+        private void UpdateCoordsPin(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "VM_Latitude" || e.PropertyName == "VM_Longitude")
+            {
+                this.myMap.Dispatcher.Invoke(System.Windows.Threading.DispatcherPriority.Normal, new Action(delegate ()
+                {
+                    AirplanePin.Location = new Location(_vm.VM_Latitude, _vm.VM_Longitude);
+                }));
+            }
+        }
+
+        // Return "Errors" label to regular color.
+        private void ErrorsBox_Opened(object sender, System.EventArgs e)
+        {
+            Color color = (Color)ColorConverter.ConvertFromString("#FFE5D5D5");
+
+            ErrorsLabel.Foreground = new SolidColorBrush(color);
+        }
+
+        /*
+            * Event handler for aileron slider object. gets called a lot.
+            * As a result, call to VM only if the difference from value is at least 0.05
+            */
+        private void AileronSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            double diff = AileronSlider.Value - _aileron;
+
+            // If diff is at least 0.05 (absolute value).
+            if (diff >= 0.05 || diff <= -0.05)
+            {
+                _aileron = AileronSlider.Value;
+                _vm.SetAileron(_aileron);
+            }
+        }
+
+        /*
+            * Event handler for throttle slider object. gets called a lot.
+            * As a result, call to VM only if the difference from value is at least 0.05
+            */
+        private void ThrottleSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            double diff = ThrottleSlider.Value - _throttle;
+
+            // If diff is at least 0.05 (absolute value).
+            if (diff >= 0.05 || diff <= -0.05)
+            {
+                _throttle = ThrottleSlider.Value;
+                _vm.SetThrottle(_throttle);
+            }
+        }
+
+        private void SetErrorEvent()
+        {
+            _vm.PropertyChanged +=
+                delegate (object sender, PropertyChangedEventArgs e)
+                {
+                    Dispatcher.Invoke(() =>
+                    {
+                        if (_vm.VM_Errors.Count > 0)
+                        {
+                            ErrorsLabel.Foreground = Brushes.Red;
+                        }
+                    });
+                };
+        }
+    }
+}
+
---
+++ PlaneController/Resources/Joystick.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PlaneController.Resources
+{
+   /// <summary>
+   /// Interaction logic for Joystick.xaml
+   /// </summary>
+   public partial class Joystick : UserControl//, INotifyPropertyChanged
+   {
+     
+      private Point initPos;
+      //public event PropertyChangedEventHandler PropertyChanged;
+      public static readonly DependencyProperty X_Property = DependencyProperty.Register(nameof(X_), typeof(double), typeof(Joystick), new PropertyMetadata((double) 0));
+      public static readonly DependencyProperty Y_Property = DependencyProperty.Register(nameof(Y_), typeof(double), typeof(Joystick), new PropertyMetadata((double) 0));
+
+
+      public Joystick()
+      {
+         InitializeComponent();
+      }
+
+      public double X_
+      {
+         get
+         {
+            //return knobPosition.X;
+            return (double)GetValue(X_Property);
+         }
+         set
+         {
+            //knobPosition.X = value;
+            SetValue(X_Property, value);
+         }
+      }
+
+      public double Y_
+      {
+         get
+         {
+            //return knobPosition.Y;
+            return (double)GetValue(Y_Property);
+         }
+         set
+         {
+            //knobPosition.Y = value;
+            SetValue(Y_Property, value);
+         }
+      }
+
+      public void centerKnob_Completed (object sender, EventArgs e)
+      {
+      }
+
+      private void Knob_Clicked(object sender, MouseButtonEventArgs e)
+      {
+         // save the coords of the mouse when the knob was clicked
+         initPos = e.GetPosition(this);
+      }
+
+      private void Knob_Moving(object sender, MouseEventArgs e)
+      {
+         if (e.LeftButton == MouseButtonState.Pressed)
+         {
+            var x = knobPosition.X;
+            var y = knobPosition.Y;
+            var maxRadius = Base.Width / 4;
+            var radius = Math.Sqrt(x * x + y * y);
+            if (radius <= maxRadius)
+            {
+               knobPosition.X = e.GetPosition(this).X - initPos.X;
+               knobPosition.Y = e.GetPosition(this).Y - initPos.Y;
+               Console.WriteLine(knobPosition.X + ",  " + knobPosition.Y);
+               X_ = knobPosition.X / maxRadius;
+               Y_ = - knobPosition.Y / maxRadius;
+
+               X_ = (X_ > 1) ? 1 : X_;
+               X_ = (X_ < -1) ? -1 : X_;
+               Y_ = (Y_ > 1) ? 1 : Y_;
+               Y_ = (Y_ < -1) ? -1 : Y_;
+               //PropertyChanged(this, new PropertyChangedEventArgs("Y_"));
+            }
+
+         }
+      }
+
+      private void Base_MouseUp(object sender, MouseButtonEventArgs e)
+      {
+         knobPosition.X = 0;
+         knobPosition.Y = 0;
+         X_ = knobPosition.X;
+         Y_ = knobPosition.Y;
+      }
+
+      private void Base_MouseLeave(object sender, MouseEventArgs e)
+      {
+         knobPosition.X = 0;
+         knobPosition.Y = 0;
+         X_ = knobPosition.X;
+         Y_ = knobPosition.Y;
+      }
+   }
+}
