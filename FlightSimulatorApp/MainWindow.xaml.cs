93e8730dd0ce91126db73f5cc3030cbb14d88b21
yehonatansofri <ysofri@gmail.com>
2020-04-18 19:20:57 +0300

    reorganized master directory

---
+++ FlightSimulatorApp/MainWindow.xaml.cs
@@ -0,0 +1,151 @@
+﻿using FlightSimulator.Model;
+using FlightSimulator.ViewModel;
+using System;
+using System.Windows;
+
+namespace FlightSimulator
+{
+    /*
+     * Class for main window - window that asks from user ip & port.
+     * There are default values so user can just click start to run app.
+     * 
+     * author: Jhonny.
+     * date: 4.1.20
+     */
+    public partial class MainWindow : Window
+    {
+        private string ip;
+        private string port;
+
+        public string IP
+        {
+            set => ip = value;
+            get => ip;
+        }
+        public string Port
+        {
+            set => port = value;
+            get => port;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            ResetData();
+        }
+
+        /*
+         * Reset ip and port to default values from application configurtion.
+         * Use helper functions.
+         */
+        public void ResetData()
+        {
+            ErrorLabel.Content = "";
+            this.ResetIP();
+            this.ResetPort();
+        }
+
+        // Reset ip to default value and update appropriate text box in window.
+        private void ResetIP()
+        {
+            IP = System.Configuration.ConfigurationManager.AppSettings["ip"];
+            IP_TB.Text = IP;
+        }
+
+        // Reset to default port and update appropriate text box in window.
+        private void ResetPort()
+        {
+            Port = System.Configuration.ConfigurationManager.AppSettings["port"];
+            Port_TB.Text = Port;
+        }
+
+        // Event handler for clicking on reset button.
+        private void Reset_Button_Click(object sender, RoutedEventArgs e)
+        {
+            ResetData();
+        }
+
+        /*
+         * Event handler for clicking on start button.
+         * If ip and port are good - run a controller window.
+         */
+        private void Start_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (ValidInput())
+            {
+                PlaneModel model = new PlaneModel();
+
+                if (Connect(model) || (ErrorLabel.Content.ToString().StartsWith("Unable")))
+                {
+                    ControllerWindow controllerWindow =
+                    new ControllerWindow(new PlaneViewModel(model));
+
+                    this.Close();
+                    controllerWindow.Show();
+                }
+                else
+                {
+                    ErrorLabel.Content = "Unable to connect to server\nClick start to move controls purposelessly";
+                }
+            }
+            else
+            {
+                ErrorLabel.Content = "Invalid IP/ Port";
+            }
+        }
+
+        // Check if IP and Port are valid.
+        private bool ValidInput()
+        {
+            bool boolean = true;
+            if (Port.Length != 4) return false;
+            try
+            {
+                if (!IsLocalHost())
+                {
+                    System.Net.IPAddress.Parse(IP);
+                }
+
+                if (!System.Text.RegularExpressions.Regex.IsMatch(port, @"^\d+$"))
+                {
+                    boolean = false;
+                }
+            }
+            catch (System.Exception)
+            {
+
+                boolean = false;
+            }
+
+            return boolean;
+        }
+
+        // Check if ip member equals to some variation of 'local host'.
+        private bool IsLocalHost()
+        {
+            if (IP == "localhost" || IP == "local host" || IP == "local_host")
+            {
+                ResetIP();
+                return true;
+            }
+
+            return false;
+        }
+
+        // Tries to connect to the client. returns true if successed.
+        private bool Connect(IPlaneModel model)
+        {
+
+            try
+            {
+                model.Connect(IP, Port);
+                return true;
+            }
+            catch (Exception)
+            {
+            }
+            return false;
+        }
+    }
+}
---
+++ FlightSimulatorApp/ViewModel/PlaneViewModel.cs
@@ -0,0 +1,106 @@
+using FlightSimulator.Model;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace FlightSimulator.ViewModel
+{
+    /*
+     * Class that serve as a ViewModel layer to transfer data in MVVM.
+     * Properties includes getters only, originating from model properties.
+     * 
+     * author: Jhonny.
+     * date: 03.28.20
+     */
+    public class PlaneViewModel : INotifyPropertyChanged
+    {
+        private IPlaneModel _model;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string VM_HeadingDeg { get { return _model.HeadingDeg.ToString("n2"); } }
+        public string VM_GPSVerticalSpeed { get { return _model.GPSVerticalSpeed.ToString("n2"); } }
+        public string VM_GPSGroundSpeed { get { return _model.GPSGroundSpeed.ToString("n2"); } }
+        public string VM_GPSAltitude { get { return _model.GPSAltitude.ToString("n2"); } }
+        public string VM_PitoSpeed { get { return _model.PitoSpeed.ToString("n2"); } }
+        public string VM_PitoAltitude { get { return _model.PitoAltitude.ToString("n2"); } }
+        public string VM_Roll { get { return _model.Roll.ToString("n2"); } }
+        public string VM_Pitch { get { return _model.Pitch.ToString("n2"); } }
+        public double VM_Latitude { get { return _model.Latitude; } }
+        public double VM_Longitude { get { return _model.Longitude; } }
+        public double VM_Rudder
+        {
+            set
+            {
+                _model.SetRudder(value);
+            }
+        }
+        public double VM_Elevator
+        {
+            set
+            {
+                _model.SetElevator(value);
+            }
+        }
+        // type is BindingList in order to allow binding with multiple threads.
+        public BindingList<string> VM_Errors { get { return new BindingList<string>(_model.Errors); } }
+
+        /*
+         * Ctor.
+         * Get model and set it a delegate to PropertyChangedEventArgs event.
+         */
+        public PlaneViewModel(IPlaneModel model)
+        {
+            _model = model;
+            _model.PropertyChanged +=
+                delegate (object sender, PropertyChangedEventArgs e)
+                {
+                    // Add "VM_" only to properties.
+                    NotifyPropertyChanged("VM_" + e.PropertyName);
+                };
+        }
+
+        // Notify View on changed properties.
+        public void NotifyPropertyChanged(string message)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(message));
+            }
+        }
+
+        // Might throw Exception.
+        public void Connect(string ip, string port)
+        {
+            _model.Connect(ip, port);
+        }
+
+        // Might throw Exception.
+        public void Disconnect()
+        {
+            _model.Disconnect();
+        }
+
+        public void SetThrottle(double value)
+        {
+            _model.SetThrottle(value);
+        }
+
+        public void SetAileron(double value)
+        {
+            _model.SetAileron(value);
+        }
+
+        public void SetRudder(double value)
+        {
+            _model.SetRudder(value);
+        }
+
+        // Valid values are [-1, 1].
+        public void SetElevator(double value)
+        {
+            _model.SetElevator(value);
+        }
+
+    }
+}
